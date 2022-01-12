0e5dcdfa7c328f694048e1e71475acf1ec663ea9
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2022-01-12 19:21:37 +0300

    Update urls.py

--- pro1/urls.py
+++ pro1/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("hello")
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', staticfiles.urls),
+     path('', home),
 ]
